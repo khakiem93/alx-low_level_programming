f88afae51806039393dd433fba77732010aedbbc
khaled <khaledabdelhakiem93@gmail.com>
2023-07-18 15:49:33 -0700

    3

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -17,7 +17,9 @@ int print_last_digit(int n)
 	if (n < 0)
 		n = -n;
 	lastDigit = n % 10;
-	_putchar(lastDigit+'0');
+	if (lastDigit < 0)
+		lastDigit = -lastDigit;
+	_putchar(lastDigit + '0');
 	return (lastDigit);
 
 }
