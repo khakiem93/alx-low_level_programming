0b7bf400d7da1cf57ef333bf252ef4f7c408cc14
khaled <khaledabdelhakiem93@gmail.com>
2023-07-18 14:17:52 -0700

    bob

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,22 @@
 #include "main.h"
-
-/**
- * main - print _putchar
+	/**
+	 * main - print _putchar
  * Return: 0
- */
+	 */
 
-int main(void)
-{
-	char x[] = "_putchar";
-	int i;
-	for (i = 0; i < 8; i++)
+
+	int main(void)
 	{
-		_putchar(x[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
-}
+	}
+
+
