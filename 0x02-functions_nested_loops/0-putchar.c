a2062dda4adf72c2b51b1c3c9f838caa2cea0855
khaled <khaledabdelhakiem93@gmail.com>
2023-07-18 13:42:53 -0700

    update_3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,14 @@
 
 /**
  * main - print _putchar
- * is written by mbah
  * Return: 0
  */
 
 int main(void)
 {
 	char x[] = "_putchar";
-	for (int i = 0; i < 8; i++)
+	int i;
+	for ( i = 0; i < 8; i++)
 	{
 		_putchar(x[i]);
 	}
