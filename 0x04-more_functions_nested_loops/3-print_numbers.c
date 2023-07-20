5a8ef9993b558435b6cf3f4c148347b06307e8ec
khaled <khaledabdelhakiem93@gmail.com>
2023-07-20 06:40:02 -0700

    5

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - check if char is digit
+ * Return: 1 if char is digit, otherwise 0.
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		_putchar('0' + n);
+	_putchar("\n");
+	return (0);
+}
