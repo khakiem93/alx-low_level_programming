569b05bf7087c092aa963753a89af445585de53d
khaled <khaledabdelhakiem93@gmail.com>
2023-07-20 06:29:40 -0700

    4

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * mul - check if char is digit
+ * @a: is the char to be checked
+ * @b: is the char to be checked
+ * Return: 1 if char is digit, otherwise 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
