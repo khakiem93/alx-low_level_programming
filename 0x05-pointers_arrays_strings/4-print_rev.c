5a6b7f35e46e5398865e0ce8adae6a0c6b81bdc2
khaled <khaledabdelhakiem93@gmail.com>
2023-07-24 11:02:00 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, followed by a new line, to stdout
+ * @s: string to print
+ */
+
+void print_rev(char *s)
+{
+	int longi = 0;
+	int x;
+
+	while (*s != '\0')
+	{
+		s++;
+		longi++;
+	}
+	s--;
+	for (x = longi; x > 0; x--)
+	{
+		_putchar(*s--);
+	}
+	_putchar('/n');
+}
