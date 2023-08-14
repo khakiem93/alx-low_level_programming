535fe225fd2adead00db61d552a4b32b1f1480e1
khaled <khaledabdelhakiem93@gmail.com>
2023-08-14 09:15:47 -0700

    1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @owner: Second member
+ * @age: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif /**DOG.h*/
